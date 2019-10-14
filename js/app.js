721d4412cdeed90cd24cd66649276415be4e280a
Ibrahim <ajarmeh1997@gmail.com>
2019-10-15 01:08:37 +0300

    fixed the hours array

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
+`use strict`;
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm',];
 var locationStore = [];
 var table = document.getElementById('cookies')
 
